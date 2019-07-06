484e8d7a84b4ee36f1178ae3d6b90c82859bcc99
amith <amith@LAPTOP-MG9I2R9V>
2019-07-06 23:09:45 +0530

    Initial Commit for location service

---
+++ src/StatlerWaldorfCorp/StatlerWaldorfCorp.LocationService/Controllers/LocationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StatlerWaldorfCorp.LocationService.Models;
+using StatlerWaldorfCorp.LocationService.Persistance;
+
+namespace StatlerWaldorfCorp.LocationService.Controllers
+{
+    [Route("locations/{memberId}")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private ILocationRecordRepository _locationRecordRepository;
+
+        public LocationController(ILocationRecordRepository locationRecordRepository)
+        {
+            _locationRecordRepository = locationRecordRepository;
+        }
+
+        [HttpPost]
+        public IActionResult AddLocation(Guid memberId,[FromBody]LocationRecord locationRecord)
+        {
+            _locationRecordRepository.Add(locationRecord);
+            return this.Created($"/locations/{memberId}/{locationRecord.ID}",
+                locationRecord);
+        }
+
+        [HttpGet]
+        public IActionResult GetLocationsForMember(Guid memberId)
+        {
+            return Ok(_locationRecordRepository.AllForMember(memberId));
+        }
+
+        [HttpGet("latest")]
+        public IActionResult GetLatestForMember(Guid memberId)
+        {
+            return Ok(_locationRecordRepository.GetLatestForMember(memberId));
+        }
+    }
+}
---
+++ src/StatlerWaldorfCorp/StatlerWaldorfCorp.LocationService/Persistance/ILocationRecordRepository.cs
@@ -0,0 +1,20 @@
+﻿using StatlerWaldorfCorp.LocationService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatlerWaldorfCorp.LocationService.Persistance
+{
+    public interface ILocationRecordRepository
+    {
+        LocationRecord Add(LocationRecord record);
+        LocationRecord Update(LocationRecord record);
+        LocationRecord Get(Guid memberId, Guid recordId);
+        LocationRecord Delete(Guid memberId, Guid recordId);
+
+        LocationRecord GetLatestForMember(Guid memberId);
+
+        ICollection<LocationRecord> AllForMember(Guid memberId);
+    }
+}
